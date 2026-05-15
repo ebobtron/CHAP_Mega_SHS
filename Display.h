360da30ca7c204c5e41a6c6f269b2d2166b0060f
ebobtron <erobclark@att.net>
2026-05-14 22:26:06 -0500

    Continued update of project files

--- Display.h
+++ Display.h
@@ -1,6 +1,10 @@
 
 /*  -----  Display.h  -----  */
 
+// this file is a container for interfacing a 800 x 480 tft display
+// providing declarations for the use of of the libraries
+// LCDWIKI_GUI and LCDWIKI_KBV from LCDWIKI
+
 
 #ifndef DISPLAY_H
 #define DISPLAY_H
@@ -17,7 +21,7 @@ void Display_Setup();
 
 void displayFauxClock(uint32_t tick);   // 86400 seconds in a day
 
-void displayLoops(uint32_t loops);  // 86400
+void displayLoops(uint32_t loops);
 
 #endif  // DISPLAy_H
 
--- README.md
+++ README.md
@@ -3,5 +3,9 @@
 Smart Home System
 1st I have not idea what I am doing.
 
-
-
+This project uses the following componets
+- Arduino Mega2560 clone  
+- MAR4018 Arduino Display Meaa2560 NT35510 
+       https://www.lcdwiki.com/4.0inch_Arduino_Display-Mega2560_NT35510
+       https://www.lcdwiki.com/res/Program/Arduino/4.0inch/Mega2560_8_16BIT_NT35510_MAR4018_V1.0/4.0inch_Arduino_Mega2560_8%2616BIT_Module_NT35510_800x480_MAR4018_V1.0.zip
+       
